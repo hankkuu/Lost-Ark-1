bfb196c69322d6c4a7c3890042aadfc3c23db294
hankkuu <https://github.com/hankkuu>
2018-10-12 19:01:21 +0900

    update

--- src/components/screens/mainTab/AuctionScreen.js
+++ src/components/screens/mainTab/AuctionScreen.js
@@ -1,15 +1,42 @@
 import React, { Component } from "react";
-import { 
+import {
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+    TouchableOpacity,
+    Image
 } from "react-native";
+import Swiper from "react-native-swiper";
 
 class AuctionScreen extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text>AuctionScreen</Text>
+                <View style={styles.notice}>
+                    <Swiper style={styles.wrapper} showsButtons={true} autoplay={true}
+                        buttonWrapperStyle={{}} paginationStyle={{ bottom: 5 }}
+                        nextButton={<Text>&gt;</Text>} prevButton={<Text>&lt;</Text>}
+                    >
+                        <View style={styles.slide}>
+                            <TouchableOpacity style={styles.link}
+                                onPress={() => this.props.navigation.navigate('Test1')}>
+                                <Image source={require("../../../../assets/shop_banner.png")} />
+                            </TouchableOpacity>
+                        </View>
+                        <View style={styles.slide}>
+                            <TouchableOpacity style={styles.link}
+                                onPress={() => this.props.navigation.navigate('Test1')}>
+                                <Image source={require("../../../../assets/lostArk2.png")} />
+                            </TouchableOpacity>
+                        </View>
+                        <View style={styles.slide}>
+                            <TouchableOpacity style={styles.link}
+                                onPress={() => this.props.navigation.navigate('Test1')}>
+                                <Image source={require("../../../../assets/lostArk3.png")} />
+                            </TouchableOpacity>
+                        </View>
+                    </Swiper>
+                </View>
             </View>
         );
     }
@@ -19,7 +46,20 @@ export default AuctionScreen;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
+        //alignItems: 'center',
+        //justifyContent: 'center'
+    },
+    notice: {
+        flexDirection: "row",
+        //justifyContent: "space-around",
+        height: 80,
+        //alignItems: 'center',
+    },
+    slide: {
+        //flex: 1,
+        //justifyContent: 'center',
+        //alignItems: 'center',
+        //width: "100%",
+        //height: 200
+    },
 });
